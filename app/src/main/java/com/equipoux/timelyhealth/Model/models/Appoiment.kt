6a5983943c0a653ad3b808b603dd345a53d9a10b
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2024-03-13 08:53:47 -0500

    Data de prueba para listar Citas medicas y el Modelo de citas medicas

---
+++ app/src/main/java/com/equipoux/timelyhealth/Model/models/Appoiment.kt
@@ -0,0 +1,14 @@
+package com.equipoUX.timelyhealth.model.models
+
+import java.util.Date
+
+
+data class Appoiment(
+    val id:Long,
+    val titulo:String,
+    val medicamento:String? = null,
+    val archivo:String? = null,
+    val fecha: Date? = null,
+    val especialidad:String? = null,
+    val descricion:String? = null,
+)
